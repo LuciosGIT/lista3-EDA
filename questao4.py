d74983fb8b9faddc1662c3af0c625339b02d3298
LuciosGIT <jloliveirajunior1903@gmail.com>
2025-11-19 22:46:39 -0300

    feat: add second implementation for pair sum existence check

--- questao4.py
+++ questao4.py
@@ -15,3 +15,17 @@ def existe_par_com_soma(lista, alvo):
 lista = [10, 5, 3, 7, 2]
 alvo = 12
 print(existe_par_com_soma(lista, alvo))
+
+
+def existe_par_com_soma2(lista, alvo):
+    for i in range(len(lista)):
+        for j in range(i + 1, len(lista)):
+            if lista[i] + lista[j] == alvo:
+                return True
+    return False
+
+
+lista = [10, 5, 3, 7, 2]
+alvo = 12
+print(existe_par_com_soma2(lista, alvo))
+
